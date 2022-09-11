537b078c652dfc136bc22440871893b4c3d5d5a8
Madison Funderburk <madfun12@gmail.com>
2022-09-11 17:39:23 -0500

    SVG stop opacity fix

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
             <p>I'm a web developer with a passion to help people expand their company's brand and to provide their customers with an unforgettable experience.</p>
             <Link href="/contact">Get in touch with me</Link>
           </div>
-          <svg viewBox="0 0 558 558" width="558" height="558" fill="none" aria-hidden="true" className={styles.spinner}><defs><linearGradient id=":R3b9m:" x1="79" y1="16" x2="105" y2="237" gradientUnits="userSpaceOnUse"><stop stopColor="#13B5C8"></stop><stop offset="1" stopColor="#13B5C8" stop-opacity="0"></stop></linearGradient></defs><path opacity=".2" d="M1 279C1 125.465 125.465 1 279 1s278 124.465 278 278-124.465 278-278 278S1 432.535 1 279Z" stroke="#13B5C8"></path><path d="M1 279C1 125.465 125.465 1 279 1" stroke="url(#:R3b9m:)" strokeLinecap="round"></path></svg>
+          <svg viewBox="0 0 558 558" width="558" height="558" fill="none" aria-hidden="true" className={styles.spinner}><defs><linearGradient id=":R3b9m:" x1="79" y1="16" x2="105" y2="237" gradientUnits="userSpaceOnUse"><stop stopColor="#13B5C8"></stop><stop offset="1" stopColor="#13B5C8" stopOpacity="0"></stop></linearGradient></defs><path opacity=".2" d="M1 279C1 125.465 125.465 1 279 1s278 124.465 278 278-124.465 278-278 278S1 432.535 1 279Z" stroke="#13B5C8"></path><path d="M1 279C1 125.465 125.465 1 279 1" stroke="url(#:R3b9m:)" strokeLinecap="round"></path></svg>
         </div>
         <section id='tech' className={`${styles.tech} ${styles.flexColumnCenter}`}>
           <h2>What I work with</h2>
