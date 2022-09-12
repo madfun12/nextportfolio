aa41935c1a8b1d5ea782dade2d2470cd3f6368f1
Madison Funderburk <madfun12@gmail.com>
2022-09-11 20:07:02 -0500

    styling revamp

--- components/topPage.js
+++ components/topPage.js
@@ -16,21 +16,12 @@ export default function TopPage(props){
 
     return(<>
         <Head>
-          <meta charset="UTF-8"></meta>
+          <meta charSet="UTF-8"></meta>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </Head>
         <div className={styles.container}>
         <header className={styles.header}>
-        <Image src="/madfunlogo.png" width={150} height={25} alt='Madison Funderburk'/>
-        <nav>
-          <ul className={styles.navholder}>
-            <li className={styles.navitem}><Link href="/"><a style={router.pathname == "/" ? {color:'rgb(63, 156, 255)'} : {}}>Home</a></Link></li>
-            <li className={styles.navitem}><Link href="/about"><a style={router.pathname == "/about" ? {color:'rgb(255, 0, 0)'} : {}}>About</a></Link></li>
-            <li className={styles.navitem}><Link href="/blog"><a style={router.pathname == "/blog" ? {color:'rgb(0, 56, 23)'} : {}}>Blog</a></Link></li>
-            <li className={styles.navitem}><Link href="/contact"><a style={router.pathname == "/contact" ? {color:'rgb(3, 100, 255)'} : {}}>Contact</a></Link></li>
-          </ul>
-        </nav>
         <div className={styles.hamburger} onClick={toggleMobile}>
           <div className={mobileActive === false ? styles.patty : styles.pattyActive}></div>
         </div>
