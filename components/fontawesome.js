decf17aeb04c95b932cf1c6138f216e1f413780c
Madison Funderburk <madfun12@gmail.com>
2022-09-27 14:58:54 -0500

    various changes

---
+++ components/fontawesome.js
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faCode,
+  faHighlighter
+);
