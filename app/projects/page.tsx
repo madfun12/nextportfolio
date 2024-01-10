b82a8317697c32ee90659b41b6259aa9ea8a22ab
Madison Funderburk <mfunderburk@teleflora.com>
2024-01-10 12:26:14 -0600

    Adding link to library app

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { BsArrowUpRightSquare } from "react-icons/bs";
 import MaxWrapper from "../components/maxWrapper";
 import SectionHeader from "../components/sectionHeader";
@@ -9,7 +10,7 @@ const Projects = () => {
                 <SectionHeader title="projects" />
                 <a
                     href="/projects/trivia-game"
-                    className="block p-4 border border-neutral-700 rounded-lg relative"
+                    className="block p-4 border border-neutral-700 rounded-lg relative mb-3"
                 >
                     <h2 className="font-bold text-2xl mb-2 dark:text-neutral-200">
                         Trivia Game
@@ -22,6 +23,27 @@ const Projects = () => {
                         This allows you to create a trivia game tailored to you.
                         Choose your options and see how sharp your trivia skills
                         are!
+                    </p>
+                </a>
+                <a
+                    href="https://library20240110102252.azurewebsites.net"
+                    className="block p-4 border border-neutral-700 rounded-lg relative mb-3"
+                >
+                    <h2 className="font-bold text-2xl mb-2 dark:text-neutral-200">
+                        My Library
+                    </h2>
+                    <BsArrowUpRightSquare
+                        className="absolute right-2 top-2 dark:text-neutral-200"
+                        size={24}
+                    />
+                    <p className="dark:text-neutral-200">
+                        A web app hosted on azure that serves as an online
+                        catalogue for my home library.
+                        <br />
+                        An ASP.Net web application using MVC architecture and
+                        that connects to an azure hosted SQL database. It's a
+                        tech stack very similar to what many large enterprises
+                        might use.
                     </p>
                 </a>
             </MaxWrapper>
