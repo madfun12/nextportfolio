0b16b913c96ceeb40558d67a29186cf73bfcae4d
Madison Funderburk <mfunderburk@teleflora.com>
2024-04-11 17:19:52 -0500

    update to mandelbrot explorer

--- public/mandelbrot-explorer/script.js
+++ public/mandelbrot-explorer/script.js
@@ -14,16 +14,17 @@ const renderMandelbrotSet = () => {
     // Go through each pixel in the canvas
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            const cx = map(x, 0, width, offsetX - 2 * zoom, offsetX + 2 * zoom);
-            const cy = map(
-                y,
+            const pixelX = map(
+                x,
                 0,
-                height,
-                offsetY - 2 * zoom,
-                offsetY + 2 * zoom
+                width,
+                offsetX - 2 * zoom,
+                offsetX + 2 * zoom
             );
+            const pixelY =
+                map(y, 0, height, offsetY - 2 * zoom, offsetY + 2 * zoom) * -1;
 
-            const iterationCount = isInMandelBrotSet(cx, cy);
+            const iterationCount = isInMandelbrotSet(pixelX, pixelY);
 
             const lightness = iterationCount === maxIterations ? 0 : 50;
             const hue = 360 * (iterationCount / maxIterations);
@@ -34,27 +35,25 @@ const renderMandelbrotSet = () => {
     }
 };
 
-const isInMandelBrotSet = (cx, cy) => {
-    let zx = 0;
-    let zy = 0;
+const isInMandelbrotSet = (pixelX, pixelY) => {
+    let x = 0;
+    let y = 0;
     let iteration = 0;
 
-    while (iteration < maxIterations) {
-        const zxtemp = zx * zx - zy * zy + cx;
-        zy = 2 * zx * zy + cy;
-        zx = zxtemp;
-        if (zx * zx + zy * zy > 4) {
-            return iteration; // Point is not in the Mandelbrot set
-        }
+    // Check if we're unbounded or if we're at maxIterations
+    while (x * x + y * y <= 4 && iteration < maxIterations) {
+        let xTemp = x * x - y * y + pixelX;
+        y = 2 * x * y + pixelY;
+        x = xTemp;
         iteration++;
     }
     return iteration;
 };
 
-// Function to convert canvas coordinates to complex plan coordinates
-function map(value, start1, stop1, start2, stop2) {
+// Function to convert canvas coordinates to complex plane coordinates
+const map = (value, start1, stop1, start2, stop2) => {
     return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
-}
+};
 
 canvas.addEventListener("wheel", (event) => {
     event.preventDefault();
@@ -107,14 +106,14 @@ iterationRangeInput.value = maxIterations;
 iterationTextInput.value = maxIterations;
 
 iterationRangeInput.addEventListener("input", (event) => {
-    maxIterations = event.target.value;
+    maxIterations = parseInt(event.target.value);
     iterationTextInput.value = maxIterations;
 
     renderMandelbrotSet();
 });
 
 iterationTextInput.addEventListener("input", (event) => {
-    maxIterations = event.target.value;
+    maxIterations = parseInt(event.target.value);
     iterationRangeInput.value = maxIterations;
 
     renderMandelbrotSet();
