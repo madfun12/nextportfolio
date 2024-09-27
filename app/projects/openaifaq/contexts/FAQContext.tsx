79352c41faba97240b4da570733a434d12c3c92d
madfun12 <86125859+madfun12@users.noreply.github.com>
2024-09-27 12:56:51 -0500

    relative path crap

--- app/projects/openaifaq/contexts/FAQContext.tsx
+++ app/projects/openaifaq/contexts/FAQContext.tsx
@@ -1,9 +1,9 @@
-import { createContext } from 'react';
-import { FAQ } from 'types/FAQ';
+import { createContext } from "react";
+import { FAQ } from "../types/FAQ";
 
 interface FAQContextType {
-    faqs: FAQ[];
-    setFAQs: React.Dispatch<React.SetStateAction<FAQ[]>>;
+  faqs: FAQ[];
+  setFAQs: React.Dispatch<React.SetStateAction<FAQ[]>>;
 }
 
 const FAQContext = createContext<FAQContextType | undefined>(undefined);
